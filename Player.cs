322bb08c24d685c2556c8e63566ae9ec1aa835d1
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-11-09 16:07:22 +0400

    add tile movement

--- Player.cs
+++ Player.cs
@@ -1,27 +1,41 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-public partial class Player : CharacterBody2D
+public partial class Player : Area2D
 {
+
+	public const int tileSize = 16;
+	private Dictionary<String, Vector2> inputs = new() {
+		{"left", Vector2.Left},
+		{"up", Vector2.Up},
+		{"down", Vector2.Down},
+		{"right", Vector2.Right},
+	};
+
 	public const float Speed = 300.0f;
 
-	public override void _PhysicsProcess(double delta)
+	public override void _Ready()
 	{
-		Vector2 velocity = Velocity;
-
-		Vector2 direction = Input.GetVector("left", "right", "forward", "backward");
-		if (direction != Vector2.Zero)
-		{
-			velocity = direction * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(velocity.X, 0f, Speed);
-			velocity.Y = Mathf.MoveToward(velocity.Y, 0f, Speed);
-
-		}
-
-		Velocity = velocity;
-		MoveAndSlide();
+		Position = Position.Snapped(Vector2.One * tileSize);
+		Position += Vector2.One * tileSize / 2;
 	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		inputs.Keys.ToList().ForEach(direction =>
+		{
+			if (@event.IsActionPressed(direction))
+			{
+				Move(direction);
+			}
+		});
+	}
+
+	private void Move(string direction)
+	{
+		GlobalPosition += inputs[direction] * tileSize;
+	}
+
 }

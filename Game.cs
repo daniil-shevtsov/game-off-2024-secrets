b072cea82b59995899704849e3e3ad4388ee7303
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-11-09 18:20:49 +0400

    water kills player

--- Game.cs
+++ Game.cs
@@ -7,6 +7,7 @@ public partial class Game : Node2D
 {
 	private Player player;
 	private TileMap tileMap;
+	private Marker2D respawnPoint;
 
 	public const int tileSize = 16;
 	private Dictionary<String, Vector2> inputs = new() {
@@ -15,21 +16,19 @@ public partial class Game : Node2D
 		{"down", Vector2.Down},
 		{"right", Vector2.Right},
 	};
-	private Dictionary<String, bool> heldInputs = new() {
-		{"left", false},
-		{"up", false},
-		{"down", false},
-		{"right", false},
-	};
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		player = GetNode<Player>("Player");
 		tileMap = (TileMap)FindChild("TileMap");
+		respawnPoint = GetNode<Marker2D>("RespawnPoint");
+		respawnPoint.GlobalPosition = respawnPoint.GlobalPosition.Snapped(Vector2.One * tileSize);
+		respawnPoint.GlobalPosition += Vector2.One * tileSize / 2;
 
-		player.Position = player.Position.Snapped(Vector2.One * tileSize);
-		player.Position += Vector2.One * tileSize / 2;
+		player.GlobalPosition = respawnPoint.GlobalPosition;
+		player.GlobalPosition = player.GlobalPosition.Snapped(Vector2.One * tileSize);
+		player.GlobalPosition += Vector2.One * tileSize / 2;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -43,12 +42,7 @@ public partial class Game : Node2D
 		{
 			if (@event.IsActionPressed(direction))
 			{
-				heldInputs[direction] = true;
 				Move(direction);
-			}
-			if (@event.IsActionReleased(direction))
-			{
-				heldInputs[direction] = false;
 			}
 		});
 	}
@@ -75,19 +69,37 @@ public partial class Game : Node2D
 		var currentTile = tileMap.LocalToMap(currentPosition);
 		var potentialTile = tileMap.LocalToMap(potentialNewPosition);
 		var finalTile = tileMap.LocalToMap(finalPosition);
-		GD.Print($"current={getTileType(currentTile)} potential={getTileType(potentialTile)} final={getTileType(finalTile)}");
+		var finalTileType = getTileType(finalTile);
+
+		GD.Print($"current={getTileType(currentTile)} potential={getTileType(potentialTile)} final={finalTileType}");
+
+		if (finalTileType == TileType.Water)
+		{
+			KillPlayer();
+		}
 	}
 
-	private String getTileType(Vector2I tileCoords)
+	private TileType? getTileType(Vector2I tileCoords)
 	{
 		var data = tileMap.GetCellTileData(0, tileCoords);
 		if (data != null)
 		{
-			return (String)data.GetCustomData("type");
+			return (TileType)Enum.Parse(typeof(TileType), (String)data.GetCustomData("type"), true);
 		}
 		else
 		{
 			return null;
 		}
+	}
+
+	private void KillPlayer()
+	{
+		Respawn();
+	}
+
+	private void Respawn()
+	{
+		GD.Print("RESPAWN");
+		player.GlobalPosition = respawnPoint.GlobalPosition;
 	}
 }
---
+++ TileType.cs
@@ -0,0 +1,6 @@
+public enum TileType
+{
+    Ground,
+    Water,
+    Wall,
+}

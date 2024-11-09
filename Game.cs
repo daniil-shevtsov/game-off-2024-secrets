5a00a25600ed7d05f06a687d4ed1e981c64d6940
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-11-09 18:09:43 +0400

    fix tile detection

--- Game.cs
+++ Game.cs
@@ -6,6 +6,7 @@ using System.Linq;
 public partial class Game : Node2D
 {
 	private Player player;
+	private TileMap tileMap;
 
 	public const int tileSize = 16;
 	private Dictionary<String, Vector2> inputs = new() {
@@ -25,6 +26,7 @@ public partial class Game : Node2D
 	public override void _Ready()
 	{
 		player = GetNode<Player>("Player");
+		tileMap = (TileMap)FindChild("TileMap");
 
 		player.Position = player.Position.Snapped(Vector2.One * tileSize);
 		player.Position += Vector2.One * tileSize / 2;
@@ -54,11 +56,38 @@ public partial class Game : Node2D
 	private void Move(string direction)
 	{
 		var potentialMove = inputs[direction] * tileSize;
+		var potentialNewPosition = player.GlobalPosition + potentialMove;
 		player.rayCast.TargetPosition = potentialMove;
 		player.rayCast.ForceRaycastUpdate();
+
+		var currentPosition = player.GlobalPosition;
+		var finalPosition = currentPosition;
 		if (!player.rayCast.IsColliding())
 		{
-			player.GlobalPosition += potentialMove;
+			finalPosition = potentialNewPosition;
+		}
+		else
+		{
+			finalPosition = currentPosition;
+		}
+		player.GlobalPosition = finalPosition;
+
+		var currentTile = tileMap.LocalToMap(currentPosition);
+		var potentialTile = tileMap.LocalToMap(potentialNewPosition);
+		var finalTile = tileMap.LocalToMap(finalPosition);
+		GD.Print($"current={getTileType(currentTile)} potential={getTileType(potentialTile)} final={getTileType(finalTile)}");
+	}
+
+	private String getTileType(Vector2I tileCoords)
+	{
+		var data = tileMap.GetCellTileData(0, tileCoords);
+		if (data != null)
+		{
+			return (String)data.GetCustomData("type");
+		}
+		else
+		{
+			return null;
 		}
 	}
 }
